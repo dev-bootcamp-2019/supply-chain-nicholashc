2c646030ed56fb1990742549a299ef9d6cb5d90e
nicholashc <44581646+nicholashc@users.noreply.github.com>
2018-11-01 16:29:21 -0700

    Update TestSupplyChain.sol
    all 17 tests passing

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -6,6 +6,9 @@ import "../contracts/SupplyChain.sol";
 
 contract TestSupplyChain {
 
+    //start with some ether
+    uint public initialBalance = 1 ether;
+
     //establish tests are working correctly and test initial sku count 
     function testInitialSkuCount() public {
       //instantiate contract 
@@ -30,82 +33,114 @@ contract TestSupplyChain {
 
     // Test for failing conditions in this contracts
     // test that every modifier is working
-    function testOnlyOwnerModifier() {
-      //instatiate contract
+    function testOnlyOwnerModifier() public {
+      //instatiate contracts
       SupplyChain s = new SupplyChain();
+      ProxyTester p = new ProxyTester();
       //declare expected value
       bool expected = true;
       //test assertion
       Assert.equal(s.accessByOwner(), expected, "msg.sender should be the owner");
-      //@nhc how do i test if this is false by sending from another address?
+      //call via proxy; should return false
+      bool proxyCallResult = p.accessByOwnerProxy(s);
+      Assert.isFalse(proxyCallResult, "accessByOwner() should throw an exception when called by non-owner");
     }
 
-    // function testVerifyCallerModifier() {
-    //   //instatiate
-    //   SupplyChain s = new SupplyChain();
-    //   //test logic
-    // }
+    //test that verify caller works in shipItem
+    //note: counterfactural covered in testShipItemNotFromSeller()
+    //note: this also tests sold modifier
+    function testVerifyCallerModifier() public {
+      //instatiate contracts
+      SupplyChain s = new SupplyChain();
+      ProxyTester p = new ProxyTester();
+      //add item
+      s.addItem("first item", 100);
+      //buy via proxy
+      p.buyProxy.value(100)(s);
+      //attempt to ship item
+      bytes memory functionSignature = padFunctionWithOneByteArgument("shipItem(uint256)", 0x00);
+      bool attemptedShipCall = address(s).call(functionSignature);
+      //check assetion
+      Assert.isTrue(attemptedShipCall, "shipItem() should not throw an exception");
+    }
 
-    // function testPaidEnoughModifier() {
-    //   //instatiate
-    //   SupplyChain s = new SupplyChain();
-    //   //test logic
-    // }
+    //note: this also tests forSale modifie
+    function testPaidEnoughModifier() public {
+      //instatiate contracts
+      SupplyChain s = new SupplyChain();
+      ProxyTester p = new ProxyTester();
+      //add via proxy
+      p.addProxy(s);
+      //attempt to buy item with exact price
+      bytes memory functionSignature = padFunctionWithOneByteArgument("buyItem(uint256)", 0x00);
+      bool attemptedBuyCall = address(s).call.value(100)(functionSignature);
+      //check assetion
+      Assert.isTrue(attemptedBuyCall, "buyItem() should not throw an exception");
+    }
 
-    // function testCheckValueModifier() {
-    //   //instatiate
-    //   SupplyChain s = new SupplyChain();
-    //   //test logic
-    // }
+    function testCheckValueModifier() public {
+      //instatiate contracts
+      SupplyChain s = new SupplyChain();
+      ProxyTester p = new ProxyTester();
+      //add via proxy
+      p.addProxy(s);
+      //attempt to buy item with more than price (excess should be returned)
+      bytes memory functionSignature = padFunctionWithOneByteArgument("buyItem(uint256)", 0x00);
+      bool attemptedBuyCall = address(s).call.value(200)(functionSignature);
+      //check assetion
+      Assert.isTrue(attemptedBuyCall, "buyItem() should not throw an exception");
+    }
 
-    // function testForSaleModifier() {
-    //   //instatiate
-    //   SupplyChain s = new SupplyChain();
-    //   //test logic
-    // }
+    function testReceivedModifier() public {
+      //instatiate contracts
+      SupplyChain s = new SupplyChain();
+      ProxyTester p = new ProxyTester();
+      //add via proxy
+      p.addProxy(s);
+      //buy
+      s.buyItem.value(100)(0);
+      //ship proxy
+      p.shipProxy(s);
+      //attempt to mark item as received
+      bytes memory functionSignature = padFunctionWithOneByteArgument("receiveItem(uint256)", 0x00);
+      bool attemptedReceiveCall = address(s).call(functionSignature);
+      //check assetion
+      Assert.isTrue(attemptedReceiveCall, "receiveItem() should not throw an exception");
+    }
 
-    // function testSoldModifier() {
-    //   //instatiate
-    //   SupplyChain s = new SupplyChain();
-    //   //test logic
-    // }
+    // buyItem
+    // test for failure if user does not send enough funds
+    function testItemPurchaseWithInsufficientFunds() public {
+      //instatiate
+      SupplyChain s = new SupplyChain();
+      //add item
+      s.addItem("first item", 100);
+      //attempt to call buy item
+      bytes memory functionSignature = padFunctionWithOneByteArgument("buyItem(uint256)", 0x00);
+      bool attemptedBuyCall = address(s).call.value(99)(functionSignature);
+      //check assetion
+      Assert.isFalse(attemptedBuyCall, "buyItem() should throw an exception");
+    }
 
-    // function testShipperModifier() {
-    //   //instatiate
-    //   SupplyChain s = new SupplyChain();
-    //   //test logic
-    // }
-
-    // function testReceivedModifier() {
-    //   //instatiate
-    //   SupplyChain s = new SupplyChain();
-    //   //test logic
-    // }
-
-    // // buyItem
-
-    // // test for failure if user does not send enough funds
-    // function testItemPurchaseWithInsufficientFunds() {
-    //   //instatiate
-    //   SupplyChain s = new SupplyChain();
-    //   //add item
-    //   s.addItem("first item", 100);
-    //   //test logic
-    //   uint expected = item[0].price;
-
-    //   Assert.equal(s.buyItem.value(99)(0), expected, "Owner should have 10000 MetaCoin initially");
-    // }
-    // // test for purchasing an item that is not for Sale
-    // function testItemPurchaseWhenNotForSale() {
-    //   //instatiate
-    //   SupplyChain s = new SupplyChain();
-    //   //test logic
-    // }
+    // test for purchasing an item that is not for Sale
+    function testItemPurchaseWhenNotForSale() public {
+      //instatiate contracts 
+      SupplyChain s = new SupplyChain();
+      ProxyTester p = new ProxyTester();
+      //add item
+      s.addItem("first item", 100);
+      //buy via proxy
+      p.buyProxy.value(100)(s);
+      //attempt to call buy item on same item
+      bytes memory functionSignature = padFunctionWithOneByteArgument("buyItem(uint256)", 0x00);
+      bool attemptedBuyCall = address(s).call.value(100)(functionSignature);
+      //check assetion
+      Assert.isFalse(attemptedBuyCall, "buyItem() should throw an exception");
+    }
 
     // shipItem
-
     // test for calls that are made by not the seller
-    function testShipItemNotFromSeller() {
+    function testShipItemNotFromSeller() public {
       //instatiate
       SupplyChain s = new SupplyChain();
       //add item
@@ -117,51 +152,104 @@ contract TestSupplyChain {
     }
 
     // test for trying to ship an item that is not marked Sold
-    function testShipItemNotYetSold() {
+    function testShipItemNotYetSold() public {
       //instatiate
       SupplyChain s = new SupplyChain();
       //add item
       s.addItem("first item", 100);
       //attempt to ship item
       bytes memory functionSignature = padFunctionWithOneByteArgument("shipItem(uint256)", 0x00);
-      bool attemptedShip = s.call(functionSignature);
+      bool attemptedShipCall = address(s).call(functionSignature);
       //establish expected value
       (,,,uint expected,,) = s.fetchItem(0); //expected state == 0 == State.ForSale
       //check assetions
-      Assert.isFalse(attemptedShip, "shipItem() should fail");
+      Assert.isFalse(attemptedShipCall, "shipItem() should throw an exception");
       Assert.equal(0, expected, "the state should be ForSale");
     }
 
-    // // receiveItem
-
-    // // test calling the function from an address that is not the buyer
-    // function testReceiveItemNotFromBuyer() {
-    //   //instatiate
-    //   SupplyChain s = new SupplyChain();
-    //   //test logic
-    // }
-    // // test calling the function on an item not marked Shipped
-    // function testReceiveItemNotYetShipped() {
-    //   //instatiate
-    //   SupplyChain s = new SupplyChain();
-    //   //test logic
-
-    // }
-
-    //utility function to encode function signature 
-    function calcFunctionSignature(string _sig) internal pure returns (bytes4) {
-      return bytes4(keccak256(abi.encodePacked(_sig)));
+    // receiveItem
+    // test calling the function from an address that is not the buyer
+    function testReceiveItemNotFromBuyer() public {
+      //instatiate contracts
+      SupplyChain s = new SupplyChain();
+      ProxyTester p = new ProxyTester();
+      //add an item
+      s.addItem("first item", 100);
+      //buy and ship item with ProxyTester
+      p.buyProxy.value(100)(s);
+      //ship item
+      s.shipItem(0);
+      //attempt to call receive item
+      bytes memory functionSignature = padFunctionWithOneByteArgument("receiveItem(uint256)", 0x00);
+      bool attemptedReceiveCall = address(s).call(functionSignature);
+      //check assetion
+      Assert.isFalse(attemptedReceiveCall, "receiveItem() should throw an exception");
     }
 
-    function padFunctionWithOneByteArgument(string _sig, bytes1 _arg) internal pure returns (bytes) {
+    // test calling the function on an item not marked Shipped
+    function testReceiveItemNotYetShipped() public {
+      //instatiate
+      SupplyChain s = new SupplyChain();
+      ProxyTester p = new ProxyTester();
+      //add item via proxy
+      p.addProxy(s);
+      //buy item
+      s.buyItem.value(100)(0);
+      //attempt to call receive item
+      bytes memory functionSignature = padFunctionWithOneByteArgument("receiveItem(uint256)", 0x00);
+      bool attemptedReceiveCall = address(s).call(functionSignature);
+      //check assetion
+      Assert.isFalse(attemptedReceiveCall, "receiveItem() should throw an exception");
+    }
+
+    //utility function to encode function signature 
+    function calcFunctionSignature(string _sig) public pure returns (bytes) {
+      return abi.encodeWithSignature(_sig);
+    }
+
+    //utility function to pad function signatures with arguments 
+    function padFunctionWithOneByteArgument(string _sig, bytes1 _arg) public pure returns (bytes) {
       bytes31 padding31bytes = 0x00000000000000000000000000000000000000000000000000000000000000;
       return abi.encodePacked(calcFunctionSignature(_sig), padding31bytes, _arg); 
     }
 
+    //contract should be able to receive ether
+    function() public payable {}
+
 }
 
-contract proxyTester {
+contract ProxyTester {
 
-    //add logic to call SupplyChain.sol from another address 
+  function addProxy(address _s) public {
+    //instatiate contract from address passed 
+    SupplyChain s = SupplyChain(_s);
+    //call add
+    s.addItem("first item", 100);
+  }
 
+  function buyProxy(address _s) public payable {
+    //instatiate contract from address passed 
+    SupplyChain s = SupplyChain(_s);
+    //call buy
+    s.buyItem.value(100)(0);
+  }
+
+  function shipProxy(address _s) public {
+    //instatiate contract from address passed 
+    SupplyChain s = SupplyChain(_s);
+    //call ship
+    s.shipItem(0);
+  }
+
+  function accessByOwnerProxy(address _s) public returns(bool){
+    //instatiate contract from address passed 
+    SupplyChain s = SupplyChain(_s);
+    //call accessByOwner
+    bytes memory functionSignature = abi.encodeWithSignature("accessByOwner()");
+    //bubble up call value 
+    return address(s).call(functionSignature);
+  }
+
+  //contract should be able to receive ether
+  function() public payable {}
 }
